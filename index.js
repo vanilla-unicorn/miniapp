d3ac1fdde371b86c0ba7ab01e28678beeea450e4
vanilla-unicorn <vanilla_unicorn@via.tokyo.jp>
2020-09-14 16:23:19 +0000

    ADDED: a prettier ignore line for issue with prettier using incorrect formatting for redis usage

--- index.js
+++ index.js
@@ -11,7 +11,8 @@ client.set('visits', 0);
 app.get('/', (req, res) => {
   client.get('visits', (err, visits) => {
     res.send(`Number of visits is ${visits}`);
-    client.set('visits', (parseInt(visits) += 1));
+    // prettier-ignore
+    client.set('visits', parseInt(visits) += 1);
   });
 });
 
